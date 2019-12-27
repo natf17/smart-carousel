3f45e9ccb187b5161efd02e3130f70e163db869d
natf17 <natfar.dev@gmail.com>
2019-12-27 13:02:05 -0500

    Bug fix

--- build/js/event_adapter.js
+++ build/js/event_adapter.js
@@ -1,6 +1,6 @@
 var carousel_variant_event_adapter = (function carousel_variant_event_adapter(globalVariable) {
 
-	$(this).on("PR_changeMatchingVariants", function(event) {
+	$(window).on("PR_changeMatchingVariants", function(event) {
 		var matchingVariants = event.detail;
 		if(matchingVariants && matchingVariants.length == 1) {
 			carousel.changeVariant(event.detail[0]);
