876c5a4c2c0285a5f56280c550d79133fd82c903
natf17 <natfar.dev@gmail.com>
2019-12-27 13:00:03 -0500

    Added code for event adapter

---
+++ build/js/event_adapter.js
@@ -0,0 +1,13 @@
+var carousel_variant_event_adapter = (function carousel_variant_event_adapter(globalVariable) {
+
+	$(this).on("PR_changeMatchingVariants", function(event) {
+		var matchingVariants = event.detail;
+		if(matchingVariants && matchingVariants.length == 1) {
+			carousel.changeVariant(event.detail[0]);
+		} else {
+			console.log("Need more details to match 1 variant!");
+		}
+		
+	});
+
+})(window);
