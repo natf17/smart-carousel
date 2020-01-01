93059404e416cff1b56f715cd12cfc75f8630faf
natf17 <natfar.dev@gmail.com>
2020-01-01 14:52:54 -0500

    Bug fix

--- build/js/event_adapter.js
+++ build/js/event_adapter.js
@@ -2,8 +2,11 @@ var carousel_variant_event_adapter = (function carousel_variant_event_adapter(gl
 
 	$(globalVariable).on("PR_changeMatchingVariants", function(event) {
 		var matchingVariants = event.detail;
+		var id = null;
 		if(matchingVariants && matchingVariants.length == 1) {
-			carousel.changeVariant(event.detail[0]);
+			id = (event.detail[0]["id"]);
+			console.log("Changing variant to " + id);
+			carousel.changeVariant(id);
 		} else {
 			console.log("Need more details to match 1 variant!");
 		}
